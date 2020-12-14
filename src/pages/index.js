5ff402709687e78f0af0ba431e726f6e63c2daac
tksat <satoko.takada@findy.co.jp>
2020-12-14 23:23:25 +0900

    first-commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">contact</Link>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
